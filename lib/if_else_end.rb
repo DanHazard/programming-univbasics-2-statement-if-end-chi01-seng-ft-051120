b9eee9ab8c59ea69c21e46facbae14f70471d151
Dan Love <daniel.love.jr@gmail.com>
2020-04-16 15:28:31 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,3 +5,13 @@ if run_code_insice
   puts "code inside"
 end
 puts "code after if...end"
+
+chance_of_rain = 0.2
+puts "let's go outside"
+if chance_of_rain > 0.5
+  puts "pack an umbreller"
+else
+  puts "enjoy the good day"
+end
+puts "don't forget the sunscreen"
+  
