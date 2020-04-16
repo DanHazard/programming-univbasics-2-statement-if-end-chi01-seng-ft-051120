1a6b8f72c16b670305ab8eb5600bdc561f416693
Dan Love <daniel.love.jr@gmail.com>
2020-04-16 15:41:12 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,31 +1,4 @@
 # Write your solution here
-run_code_insice = false
-puts "code before if...end"
-if run_code_insice
-  puts "code inside"
-end
-puts "code after if...end"
-
-chance_of_rain = 1
-puts "let's go outside"
-if chance_of_rain > 0.5
-  puts "pack an umbreller"
-else
-  puts "enjoy the good day"
-end
-puts "don't forget the sunscreen"
-
-# ex3
-chance_of_rain = 0.8
-if chance_of_rain <= 0.25
-  puts "pack a shelter"
-elsif (chance_of_rain >0.25 && chance_of_rain <0.75)
-  puts "pack an umbreller"
-else
-  puts "stay home and read LeGuin"
-end
-
-# ex4
 cur_time = Time.now
 if cur_time.to_i % 2 == 0 
   puts "Even!"
